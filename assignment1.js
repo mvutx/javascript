d61c0c714911684c738f59b786a0e71d45d18e83
vutii <giftmuuo.com@gmail.com>
2026-02-25 19:15:00 +0300

    update

---
+++ assignment1.js
@@ -0,0 +1,23 @@
+// object 
+let student = {
+    name: "Alice",
+    age: 20,
+    course: "Computer Science",
+    isRegistered: true
+};
+//accessing the items
+console.log(student.name);
+console.log(student.age);
+// method two 
+console.log(student["course"]);
+console.log(student["isRegistered"]);
+
+// array data type
+let fruits = ["Apple", "Mango", "Orange", "Banana"];
+//accessing 
+console.log(fruits[0]);  
+console.log(fruits[2]);  
+
+//changing array
+fruits[1] = "Pineapple";
+console.log(fruits);
